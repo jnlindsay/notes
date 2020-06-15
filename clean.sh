894336f00347d04e17c966b36a83c0c55ffb7ef2
jnlindsay <jeremy@jeremylindsay.com>
2020-06-15 14:11:11 +1000

    updated gitignore and clean file

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-find . -regextype posix-egrep -regex ".*\.(aux|bcf|bbl|blg|xml|gz|toc|log|pdf|out)$" -type f -delete
+find . -regextype posix-egrep -regex ".*\.(aux|bcf|bbl|blg|xml|gz|toc|log|pdf|out|fls|fdb_latexmk)$" -type f -delete
